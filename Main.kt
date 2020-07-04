45c0068e26023bcdaf8bfe8a40cf952123bc484a
Adin Horovitz <adin@tapclicks.com>
2020-07-03 17:40:54 -0700

    regex input logic working

--- Main.kt
+++ Main.kt
@@ -1,17 +1,34 @@
 package converter
 
 fun main() {
+
+    val regex = "(\\d+\\.?\\d*) (degrees? )?([a-z]*) [a-z]* (degrees? )?([a-z]*)".toRegex(RegexOption.IGNORE_CASE)
+
     do {
         print("Enter what you want to convert (or exit): ")
-        val input = readLine()!!.split(" ")
+        val input = readLine()!!
+        val matches = regex.find(input)
 
-        if(input.size == 4) {
-            val inputMagnitude = input[0].toDouble()
-            val inputUnit = input[1].toLowerCase()
-            val outputUnit = input[3].toLowerCase()
-            val converter = UnitConverter.getConverter(inputUnit, outputUnit)
-
-            println(converter.convert(inputMagnitude, inputUnit, outputUnit))
+        if (matches == null) {
+            println("Parse error")
+            continue
         }
-    } while(input[0] != "exit")
+
+        /*
+         * Group values:
+         * 0 -> entire string input (if it matches)
+         * 1 -> input magnitude
+         * 2 -> degree string (ignore)
+         * 3 -> input unit
+         * 4 -> degree string (ignore)
+         * 5 -> output unit
+         */
+        val inputMagnitude = matches.groupValues[1].toDouble()
+        val inputUnit = matches.groupValues[3].toLowerCase()
+        val outputUnit = matches.groupValues[5].toLowerCase()
+
+        val converter = UnitConverter.getConverter(inputUnit, outputUnit)
+        println(converter.convert(inputMagnitude, inputUnit, outputUnit))
+
+    } while (input != "exit")
 }
